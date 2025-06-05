8518e930bc6480d59c61d911a0d8187b064ab65b
ali-intervest <ali.n@intervest.lk>
2025-06-05 13:25:02 +0530

    add

--- scripts/install_jenkins.sh
+++ scripts/install_jenkins.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 yum update -y
+
+  # Install Docker
+yum install -y docker
+systemctl start docker
+systemctl enable docker
+usermod -aG docker jenkins
+sudo usermod -s /bin/bash jenkins
+
+reboot
+
+sudo yum install git -y
+
 rpm --import https://yum.corretto.aws/corretto.key
 curl -Lo /etc/yum.repos.d/corretto.repo https://yum.corretto.aws/corretto.repo
 yum install -y java-17-amazon-corretto
-wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 
 
 wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 curl -fsSL https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key | sudo tee /etc/pki/rpm-gpg/jenkins.io.key
 sudo rpm --import /etc/pki/rpm-gpg/jenkins.io.key
-
 yum install -y jenkins
+
 systemctl enable jenkins
 systemctl start jenkins
