89fe8b00aba6bbea646666266099b7d2f54773c5
ali-intervest <ali.n@intervest.lk>
2025-06-11 15:33:28 +0530

    add

--- hello-world-app/app.py
+++ hello-world-app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, world from ECS and Jenkins CI/CD 🚀"
+    return "Hello, world from ECS and Jenkins CI/CD 🚀 By Ali Naleem"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
--- modules/jenkins_ec2/main.tf
+++ modules/jenkins_ec2/main.tf
@@ -32,6 +32,13 @@ resource "aws_security_group" "jenkins_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -52,7 +59,6 @@ resource "aws_instance" "jenkins" {
   iam_instance_profile        = var.iam_instance_profile
   vpc_security_group_ids      = [aws_security_group.jenkins_sg.id]
   associate_public_ip_address = true
-
   user_data = file("${path.module}/../../scripts/install_jenkins.sh")
 
   tags = {
